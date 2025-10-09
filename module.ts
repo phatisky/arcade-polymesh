a687716ff6f23d31f1c994757f31aba455cdaae0
Phatrisky NioryuXenris <naphatism@hotmail.com>
2025-10-09 17:09:20 +0700

    Update module.ts

--- module.ts
+++ module.ts
@@ -40,13 +40,13 @@ namespace Polymesh {
     //% weight=9
     export function renderAll(id: number, output: Image, linecolor?: number) {
         if ((id == null || isNaN(id)) || !output) return;
-        const sorted = Polymesh.__mesh[id].filter( msh => !msh || !msh.isDel() ).map(msh => ({ mesh: msh, depth: meshDepthZ(msh) }));
+        const sorted = Polymesh.__mesh[id].filter( msh => !msh || !msh.isDel()).map(msh => ({ mesh: msh, depth: meshDepthZ(msh) }));
         if (sorted.length <= 0) return;
         switch (sort) {
             case 0x0: sorted.sort((a, b) => b.depth - a.depth);          break;
-            case 0x1: duoQuickSort(sorted, (a, b) => b.depth - a.depth); break;
-            case 0x2:
-            default:  quickSort(sorted, (a, b) => b.depth - a.depth);    break;
+            case 0x1:
+            default:  duoQuickSort(sorted, (a, b) => b.depth - a.depth); break;
+            case 0x2: quickSort(sorted, (a, b) => b.depth - a.depth);    break;
         }
         for (const m of sorted) if (!m.mesh.flag.invisible) render(m.mesh, output, linecolor);
     }
@@ -104,9 +104,9 @@ namespace Polymesh {
         const tris = msh.faces.slice();
         switch (sort) {
             case 0x0: tris.sort((a, b) => avgZ(rotated, b.indices) - avgZ(rotated, a.indices));          break;
-            case 0x1: duoQuickSort(tris, (a, b) => avgZ(rotated, b.indices) - avgZ(rotated, a.indices)); break;
-            case 0x2:
-            default:  quickSort(tris, (a, b) => avgZ(rotated, b.indices) - avgZ(rotated, a.indices));    break;
+            case 0x1:
+            default:  duoQuickSort(tris, (a, b) => avgZ(rotated, b.indices) - avgZ(rotated, a.indices)); break;
+            case 0x2: quickSort(tris, (a, b) => avgZ(rotated, b.indices) - avgZ(rotated, a.indices));    break;
         }
 
         // Render
