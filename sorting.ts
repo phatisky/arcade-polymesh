40a2d5fb6aeb37c35db21117bb5354278da8e163
Phatrisky NioryuXenris <naphatism@hotmail.com>
2025-10-10 07:52:27 +0700

    Update sorting.ts

--- sorting.ts
+++ sorting.ts
@@ -1,29 +1,20 @@
 
 namespace Polymesh {
 
-    const median3 = <T>(arr: T[], cmp: (a: T, b: T) => number, l: number, r: number) => {
-        const m = (l + r) >> 1;
-        if (cmp(arr[l], arr[m]) > 0) swap(arr, l, m);
-        if (cmp(arr[l], arr[r]) > 0) swap(arr, l, r);
-        if (cmp(arr[m], arr[r]) > 0) swap(arr, m, r);
-        return m;
-    };
-
     const duoPartition = <T>(arr: T[], cmp: (a: T, b: T) => number, lo: number, hi: number) => {
-        //const mi = median3(arr, cmp, lo, hi); swap(arr, lo, mi)
         if (cmp(arr[lo], arr[hi]) > 0) swap(arr, lo, hi);
         const p = arr[lo], q = arr[hi]; 
-        let l = lo + 1, r = hi - 1;
-        for (let k = l; k <= r; k++) {
-            if (cmp(arr[k], p) < 0) swap(arr, k, l++);
+        let lp = lo + 1, rp = hi - 1;
+        for (let k = lp; k <= rp; k++) {
+            if (cmp(arr[k], p) < 0) swap(arr, k, lp++);
             else if (cmp(arr[k], q) > 0) {
-                while (cmp(arr[r], q) > 0 && k < r) r--;
-                swap(arr, k, r--);
-                if (cmp(arr[k], p) < 0) swap(arr, k, l++);
+                while (cmp(arr[rp], q) > 0 && k < rp) rp--;
+                swap(arr, k, rp--);
+                if (cmp(arr[k], p) < 0) swap(arr, k, lp++);
             }
         }
-        swap(arr, lo, --l), swap(arr, hi, ++r);
-        return [l, r];
+        swap(arr, lo, --lp), swap(arr, hi, ++rp);
+        return [lp, rp];
     }
 
     // --- duoQuickSort ---
@@ -31,7 +22,6 @@ namespace Polymesh {
         const stack: number[][] = [[0, arr.length - 1]];
         while (stack.length > 0) {
             let [lo, hi] = stack.pop();
- 
             while (lo < hi) {
                 const [lp, rp] = duoPartition(arr, cmp, lo, hi);
                 // Middle Subarray (lp+1 to rp-1)
@@ -50,10 +40,10 @@ namespace Polymesh {
     };
 
     export function quickSort<T>(arr: T[], cmp: (a: T, b: T) => number) {
-        // Stack 2D: [lo, hi, depth]
+        // Stack 2D: [lo, hi]
         const stack: number[][] = [[0, arr.length - 1]];
         while (stack.length > 0) {
-            let [lo, hi] = stack.pop(), size = hi - lo + 1;
+            let [lo, hi] = stack.pop();
             while (lo < hi) {
                 // Partition for iterative
                 const p = partition(arr, cmp, lo, hi);
