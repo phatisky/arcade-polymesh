413f8e0eba94109829480fe7e5dc8b0d0c182389
Phatrisky NioryuXenris <naphatism@hotmail.com>
2025-10-10 19:35:43 +0700

    Update me.ts

--- me.ts
+++ me.ts
@@ -5,6 +5,9 @@ namespace Polymesh {
     export const __mesh: polymesh[][] = []
     export const PHI = 1.6180339887, NORMAL_DIST = 1.665, LOD_DIST = 1.06
 
+    export function __mesh_del(msh: polymesh) { Polymesh.__mesh[msh.kind][msh.kind_idx] = null; }
+    export function __mesh_upd(msh: polymesh) { Polymesh.__mesh[msh.kind][msh.kind_idx] = msh; }
+
     export let ax   = 0, az   = 0,   ay   = 0,   avx     = 0, avy   = 0, avz   = 0, aax   = 0, aay   = 0, aaz   = 0, afx   = 0, afy   = 0, afz   = 0
     export let camx = 0, camy = 0,   camz = 0,   camvx   = 0, camvy = 0, camvz = 0, camax = 0, camay = 0, camaz = 0, camfx = 0, camfy = 0, camfz = 0
     export let zoom = 1, sort = 0x0, dist = 150, fardist = 0
