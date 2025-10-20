57b8e643818432c8d4e4c1252908716d3ffdfc94
Phatisky NioryuXenris <naphatism@hotmail.com>
2025-10-20 17:15:48 +0700

    Implement Motion3 interface for angle and cam

--- me.ts
+++ me.ts
@@ -2,11 +2,14 @@
 //% block="Poly mesh" color="#279139" icon="\uf1b2" groups='["Create","Controls","Styling"]'
 namespace Polymesh {
 
+    interface Motion3 { x: number, y: number, z: number, vx: number, vy: number, vz: number, ax: number, ay: number, az: number, fx: number, fy: number, fz: number }
+    interface Vector3 { x: number, y: number, z: number }
+
     export const __mesh: polymesh[][] = []
     export const PHI = 1.6180339887, NORMAL_DIST = 1.665, LOD_DIST = 1.06
 
-    export const angle = { x: 0, y: 0, z: 0, vx: 0, vy: 0, vz: 0, ax: 0, ay: 0, az: 0, fx: 0, fy: 0, fz: 0 };
-    export const cam = { x: 0, y: 0, z: 0, vx: 0, vy: 0, vz: 0, ax: 0, ay: 0, az: 0, fx: 0, fy: 0, fz: 0 };
+    export const angle: Motion3 = { x: 0, y: 0, z: 0, vx: 0, vy: 0, vz: 0, ax: 0, ay: 0, az: 0, fx: 0, fy: 0, fz: 0 };
+    export const cam: Motion3 = { x: 0, y: 0, z: 0, vx: 0, vy: 0, vz: 0, ax: 0, ay: 0, az: 0, fx: 0, fy: 0, fz: 0 };
     export let zoom = 1, sort = 0x0, dist = 150, fardist = 0
 
     //% blockId=poly_sorttype
