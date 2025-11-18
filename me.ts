c8f647d55dc045a92bf944093b7164a03e4244a0
Phatisky NioryuXenris <naphatism@hotmail.com>
2025-11-18 13:22:55 +0700

    Refactor mesh kind management functions
    Refactor mesh handling functions to use __meshes_ref for better management of mesh kinds and indices.

--- me.ts
+++ me.ts
@@ -8,20 +8,25 @@ namespace Polymesh {
     export interface Vector3 { x: number, y: number, z: number }
 
     const __meshes: polymesh[] = [];
+    const __meshes_ref: {[id: number]: number[]} = {};
     export const PHI = 1.6180339887, NORMAL_DIST = 1.665, LOD_DIST = 1.2
 
     export const angle: Motion3 = { x: 0, y: 0, z: 0, vx: 0, vy: 0, vz: 0, ax: 0, ay: 0, az: 0, fx: 0, fy: 0, fz: 0 };
     export const cam: Motion3 = { x: 0, y: 0, z: 0, vx: 0, vy: 0, vz: 0, ax: 0, ay: 0, az: 0, fx: 0, fy: 0, fz: 0 };
     export let zoom = 1, sort = 0x0, dist = 150, fardist = 0;
 
-    export function __upd_kind(msh: polymesh, id: number) {
-        Polymesh.__mesh[convertToText(msh)] = null;
-        this.kind = Math.floor(id);
-        const kind = this.kind;
-        if (!Polymesh.__mesh[kind]) __mesh_item[kind] = [], __mesh_hash[kind] = {};
-        const hasKind = __mesh_hash[kind][convertToText(null)]
-        if (!hasKind) __mesh_item[kind].push(null), __mesh_hash[kind][convertToText(null)] = true;
-        this.kind_idx = (kind_idx >= 0) ? kind_idx : Polymesh.__mesh[kind].length - 1
+    export function __meshes_upd_kind(msh: polymesh, kind: number) {
+        if (this.kind === Math.floor(kind)) return;
+        __meshes_ref[msh.kind].removeAt(__meshes_ref[msh.kind].indexOf(msh.idx));
+        this.kind = Math.floor(kind);
+        if (!__meshes_ref[kind]) __meshes_ref[kind] = []
+        __meshes_ref[msh.kind].push(msh.idx);
+    }
+
+    export function __meshes_del(msh: polymesh) {
+        __meshes_ref[msh.kind].removeAt(__meshes_ref[msh.kind].indexOf(msh.idx));
+        __meshes[msh.idx] = null;
+        
     }
 
     //% blockId=poly_sorttype
@@ -38,12 +43,15 @@ namespace Polymesh {
     //% group="create"
     //% weight=10
     export function create(kind: number) {
+        if (!__meshes_ref[kind]) __meshes_ref[kind] = []
         let idx = __meshes.indexOf(null)
         if (idx < 0) {
             idx = __meshes.length
+            __meshes_ref[kind].push(idx)
             __meshes.push(new polymesh(Math.floor(kind), idx);
             return __meshes[idx]
         }
+        __meshes_ref[kind].push(idx)
         __meshes[idx] = new polymesh(Math.floor(kind), idx);
         return __meshes[idx]
     }
@@ -54,7 +62,7 @@ namespace Polymesh {
     //% group="mesh kind"
     //% weight=13
     export function meshAll(kind?: number) {
-        return Polymesh.__mesh[kind].filter( msh => msh != null || (msh && !msh.isDel()))
+        return __meshes_ref[kind].map(i => __meshes[i])
     }
 
 }
