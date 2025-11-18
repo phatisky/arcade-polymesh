aee6359b80cbf9e964a4ecd1ddc92d08fd2e0018
Phatisky NioryuXenris <naphatism@hotmail.com>
2025-11-18 14:06:51 +0700

    Refactor motion and angle functions for simplification

--- myself.ts
+++ myself.ts
@@ -52,7 +52,7 @@ namespace Polymesh {
         }
     }
 
-    export function getMotion(motion: Motion3, choice: uint8, x: number) {
+    export function getMotion(motion: Motion3, choice: uint8) {
         switch (choice) {
             case 0x0: return motion.x;
             case 0x1: return motion.y;
@@ -85,20 +85,7 @@ namespace Polymesh {
     //% group="main angle"
     //% weight=5
     export function changeAngle(choice: PolyAngle, x: number) {
-        switch (choice) {
-            case 0x0: if (angle.x  !== angle.x  + x) angle.x  += x; break
-            case 0x1: if (angle.y  !== angle.y  + x) angle.y  += x; break
-            case 0x2: if (angle.z  !== angle.z  + x) angle.z  += x; break
-            case 0x3: if (angle.vx !== angle.vx + x) angle.vx += x; break
-            case 0x4: if (angle.vy !== angle.vy + x) angle.vy += x; break
-            case 0x5: if (angle.vz !== angle.vz + x) angle.vz += x; break
-            case 0x6: if (angle.ax !== angle.ax + x) angle.ax += x; break
-            case 0x7: if (angle.ay !== angle.ay + x) angle.ay += x; break
-            case 0x8: if (angle.az !== angle.az + x) angle.az += x; break
-            case 0x9: if (angle.fx !== angle.fx + x) angle.fx += x; break
-            case 0xA: if (angle.fy !== angle.fy + x) angle.fy += x; break
-            case 0xB: if (angle.fz !== angle.fz + x) angle.fz += x; break
-        }
+        changeMotion(angle, choice, x)
     }
     //% blockId=poly_camera_change
     //% block="change $choice by $x"
@@ -106,42 +93,18 @@ namespace Polymesh {
     //% weight=5
     export function changeCam(choice: PolyCam, x: number) {
         switch (choice) {
-            case 0x0: default: if (zoom    !== zoom    + x) zoom    += x; break
-            case 0x1:          if (dist    !== dist    + x) dist    += x; break
-            case 0x2:          if (fardist !== fardist + x) fardist += x; break
-            case 0x3:          if (cam.x   !== cam.x   + x) cam.x   += x; break
-            case 0x4:          if (cam.y   !== cam.y   + x) cam.y   += x; break
-            case 0x5:          if (cam.z   !== cam.z   + x) cam.z   += x; break
-            case 0x6:          if (cam.vx  !== cam.vx  + x) cam.vx  += x; break
-            case 0x7:          if (cam.vy  !== cam.vy  + x) cam.vy  += x; break
-            case 0x8:          if (cam.vz  !== cam.vz  + x) cam.vz  += x; break
-            case 0x9:          if (cam.ax  !== cam.ax  + x) cam.ax  += x; break
-            case 0xA:          if (cam.ay  !== cam.ay  + x) cam.ay  += x; break
-            case 0xB:          if (cam.az  !== cam.az  + x) cam.az  += x; break
-            case 0xC:          if (cam.fx  !== cam.fx  + x) cam.fx  += x; break
-            case 0xD:          if (cam.fy  !== cam.fy  + x) cam.fy  += x; break
-            case 0xE:          if (cam.fz  !== cam.fz  + x) cam.fz  += x; break
+            case 0xC: if (zoom    !== zoom    + x) zoom    += x; return
+            case 0xD: if (dist    !== dist    + x) dist    += x; return
+            case 0xE: if (fardist !== fardist + x) fardist += x; return
         }
+        changeMotion(cam, choice, x)
     }
     //% blockId=poly_angle_set
     //% block="set $choice to $x"
     //% group="main angle"
     //% weight=10
     export function setAngle(choice: PolyAngle, x: number) {
-        switch (choice) {
-            case 0x0: if (angle.x  !== x) angle.x  = x; break
-            case 0x1: if (angle.y  !== x) angle.y  = x; break
-            case 0x2: if (angle.z  !== x) angle.z  = x; break
-            case 0x3: if (angle.vx !== x) angle.vx = x; break
-            case 0x4: if (angle.vy !== x) angle.vy = x; break
-            case 0x5: if (angle.vz !== x) angle.vz = x; break
-            case 0x6: if (angle.ax !== x) angle.ax = x; break
-            case 0x7: if (angle.ay !== x) angle.ay = x; break
-            case 0x8: if (angle.az !== x) angle.az = x; break
-            case 0x9: if (angle.fx !== x) angle.fx = x; break
-            case 0xA: if (angle.fy !== x) angle.fy = x; break
-            case 0xB: if (angle.fz !== x) angle.fz = x; break
-        }
+        setMotion(angle, choice, x)
     }
     //% blockId=poly_camera_set
     //% block="set $choice to $x"
@@ -149,22 +112,11 @@ namespace Polymesh {
     //% weight=10
     export function setCam(choice: PolyCam, x: number) {
         switch (choice) {
-            case 0x0: default: if (zoom    !== x) zoom    = x; break
-            case 0x1:          if (dist    !== x) dist    = x; break
-            case 0x2:          if (fardist !== x) fardist = x; break
-            case 0x3:          if (cam.x   !== x) cam.x   = x; break
-            case 0x4:          if (cam.y   !== x) cam.y   = x; break
-            case 0x5:          if (cam.z   !== x) cam.z   = x; break
-            case 0x6:          if (cam.vx  !== x) cam.vx  = x; break
-            case 0x7:          if (cam.vy  !== x) cam.vy  = x; break
-            case 0x8:          if (cam.vz  !== x) cam.vz  = x; break
-            case 0x9:          if (cam.ax  !== x) cam.ax  = x; break
-            case 0xA:          if (cam.ay  !== x) cam.ay  = x; break
-            case 0xB:          if (cam.az  !== x) cam.az  = x; break
-            case 0xC:          if (cam.fx  !== x) cam.fx  = x; break
-            case 0xD:          if (cam.fy  !== x) cam.fy  = x; break
-            case 0xE:          if (cam.fz  !== x) cam.fz  = x; break
+            case 0xC: if (zoom    !== x) zoom    = x; return
+            case 0xD: if (dist    !== x) dist    = x; return
+            case 0xE: if (fardist !== x) fardist = x; return
         }
+        setMotion(cam, choice, x)
     }
 
     //% blockId=poly_angle_get
@@ -172,21 +124,7 @@ namespace Polymesh {
     //% group="main angle"
     //% weight=4
     export function getAngle(choice: PolyAngle) {
-        switch (choice) {
-            case 0x0: return angle.x
-            case 0x1: return angle.y
-            case 0x2: return angle.z
-            case 0x3: return angle.vx
-            case 0x4: return angle.vy
-            case 0x5: return angle.vz
-            case 0x6: return angle.ax
-            case 0x7: return angle.ay
-            case 0x8: return angle.az
-            case 0x9: return angle.fx
-            case 0xA: return angle.fy
-            case 0xB: return angle.fz
-        }
-        return NaN
+        return getMotion(angle, choice)
     }
 
     //% blockId=poly_camera_get
@@ -195,23 +133,11 @@ namespace Polymesh {
     //% weight=4
     export function getCam(choice: PolyCam) {
         switch (choice) {
-            case 0x0: default: return zoom
-            case 0x1:          return dist
-            case 0x2:          return fardist
-            case 0x3:          return cam.x
-            case 0x4:          return cam.y
-            case 0x5:          return cam.z
-            case 0x6:          return cam.vx
-            case 0x7:          return cam.vy
-            case 0x8:          return cam.vz
-            case 0x9:          return cam.ax
-            case 0xA:          return cam.ay
-            case 0xB:          return cam.az
-            case 0xC:          return cam.fx
-            case 0xD:          return cam.fy
-            case 0xE:          return cam.fz
+            case 0xC: return zoom
+            case 0xD: return dist
+            case 0xE: return fardist
         }
-        return NaN
+        return getMotion(cam, choice, x)
     }
 
 }
