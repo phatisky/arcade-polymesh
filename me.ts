663fe77bc551af47fc05eecbd7441f26025a3c1e
Phatisky NioryuXenris <naphatism@hotmail.com>
2025-11-19 08:31:11 +0700

    Correct filter method in mesh update and delete functions
    Fix typo in filter method for removing mesh indices.

--- me.ts
+++ me.ts
@@ -18,16 +18,15 @@ namespace Polymesh {
 
     export function __meshes_upd_kind(msh: polymesh, kind: number) {
         if (msh.kind === Math.floor(kind)) return;
-        __meshes_ref[msh.kind].removeAt(__meshes_ref[msh.kind].indexOf(msh.idx));
+        __meshes_ref[msh.kind] = __meshes_ref[msh.kind].fillter(idx => idx !== msh.idx);
         msh.kind = Math.floor(kind);
         if (!__meshes_ref[msh.kind]) __meshes_ref[msh.kind] = []
         __meshes_ref[msh.kind].push(msh.idx);
     }
 
     export function __meshes_del(msh: polymesh) {
-        __meshes_ref[msh.kind].removeAt(__meshes_ref[msh.kind].indexOf(msh.idx));
+        __meshes_ref[msh.kind] = __meshes_ref[msh.kind].fillter(idx => idx !== msh.idx);
         __meshes[msh.idx] = null;
-        
     }
 
     //% blockId=poly_sorttype
